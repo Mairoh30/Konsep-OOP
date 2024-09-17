0f26a651bc6599c5018db00b6ffff22cde9e7ded
H P <nurchumairohahmad@gmail.com>
2024-09-17 11:48:18 +0700

    konsep-oop

---
+++ src/main/kotlin/id/ac/polbeng/mairoh/test_kelas/Enkapsulasi.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.mairoh.test_kelas
+
+class Address {
+    var name: String = "Holmes, Sherlock"
+    var street: String = "Baker"
+    var city: String = "London"
+    var state: String? = null
+    var zip: String = "123456"
+}
